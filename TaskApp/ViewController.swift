001728f7db0c3296d08d50d0393010027ca0994d
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-29 12:56:49 +0500

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by Junaid on 29/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
