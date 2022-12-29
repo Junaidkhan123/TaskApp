56a581fb6cb1caf7597a13ca6a989973b595c4c6
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-29 14:09:43 +0500

    added MenuSection Enum

---
+++ TaskApp/Model/MenuSection.swift
@@ -0,0 +1,12 @@
+//
+//  MenuSection.swift
+//  TaskApp
+//
+//  Created by Junaid on 29/12/2022.
+//
+
+import Foundation
+enum MenuSection: String, CaseIterable {
+    case ongoing = "Ongoing"
+    case done = "Done"
+}
