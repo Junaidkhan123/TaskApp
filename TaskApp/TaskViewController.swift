0c86d0ecbd008d927a8e320194edeb65af1a1653
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-29 14:14:35 +0500

    SegmentedControler setup done

--- TaskApp/TaskViewController.swift
+++ TaskApp/TaskViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class TaskViewController: UIViewController {
-
+    @IBOutlet weak var segmmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupSegmentedControl()
     }
 
+    private func setupSegmentedControl() {
+        segmmentedControl.removeAllSegments()
+
+        MenuSection.allCases.enumerated().forEach { index, section in
+            segmmentedControl.insertSegment(withTitle: section.rawValue, at: index, animated: false)
+        }
+
+        segmmentedControl.selectedSegmentIndex = 0
+    }
 
 }
 
