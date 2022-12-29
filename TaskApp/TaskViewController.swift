5f8736da8294e14e423d9b1155d8530b42b81df0
JunaidKhan8855 <junaid.khan@digitify.com>
2022-12-29 13:31:20 +0500

    Changed the background color and Renamed the ViewController

--- TaskApp/TaskViewController.swift
+++ TaskApp/TaskViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskViewController.swift
 //  TaskApp
 //
 //  Created by Junaid on 29/12/2022.
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.view.backgroundColor = .yellow
     }
 
 
